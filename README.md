0de4674dca923349cf20a904b6fa03f452ebb28b
Phat X Luong <66936482+phatx88@users.noreply.github.com>
2020-10-09 08:47:52 +0700

    add source-code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Love_message_HTML
 lời tỏ tình của chàng code đơn thân :)
+
+this web page was designed based on love tree concept and source code from the link below
+
+- [HTML5 Love Tree](http://blog.csdn.net/lisenyang/article/details/35220823)
---
+++ js/functions.js
@@ -0,0 +1,63 @@
+$(function () {
+	$(".tricky").on({
+		mouseover: function () {
+			$(this).css({
+				left: (Math.random() * 100) + "%",
+				top: (Math.random() * 100) + "%",
+			});
+		}
+	});
+});
+
+$('.tricky').bind('touchstart', function () {
+	$(this).css({
+		left: (Math.random() * 100) + "%",
+		top: (Math.random() * 100) + "%"
+	});
+})
+
+
+// variables
+var $win = $(window);
+var clientWidth = $win.width();
+var clientHeight = $win.height();
+
+$(window).resize(function () {
+	var newWidth = $win.width();
+	var newHeight = $win.height();
+	if (newWidth != clientWidth && newHeight != clientHeight) {
+		location.replace(location);
+	}
+});
+
+
+
+(function ($) {
+	$.fn.typewriter = function () {
+		this.each(function () {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			$ele.show();
+			var timer = setInterval(function () {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+					while (str.substr(progress, 1) == "\t")
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+					$('div.btn-wrap').toggleClass('display');
+					$('div.btn-wrap').addClass('animated');
+				}
+			}, 75);
+		});
+		
+		return this;
+	};
+})(jQuery);
+
+
